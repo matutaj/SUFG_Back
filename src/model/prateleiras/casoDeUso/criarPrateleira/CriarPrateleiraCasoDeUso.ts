bf531175520a4f4fd3f67cb0d40da9fd0c313334
matutaj <matutajorge2002@gmail.com>
2025-03-11 22:08:50 +0100

    Metodo para criar prateleira

--- src/model/prateleiras/casoDeUso/criarPrateleira/CriarPrateleiraCasoDeUso.ts
+++ src/model/prateleiras/casoDeUso/criarPrateleira/CriarPrateleiraCasoDeUso.ts
@@ -1,16 +1,23 @@
-import { DadosPrateleira } from "../repositorioPrateleira/IPrateleira";
-import { PrateleiraRepositorio } from "../repositorioPrateleira/implementacoes/RepositorioPrateleira";
+import { DadosPrateleira } from "../../repositorioPrateleira/IPrateleira";
+import { PrateleiraRepositorio } from "../../repositorioPrateleira/implementacoes/RepositorioPrateleira";
 import { prateleiras } from "@prisma/client";
 class CriarPrateleiraCasoDeUso {
-    async execute({descricaoPrateleira, nomePrateleira}: DadosPrateleira): Promise<prateleiras> {
-        const prateleiraRepositorio = new PrateleiraRepositorio()
-        const existeNome = await prateleiraRepositorio.listarUmaPrateleiraPeloNome(nomePrateleira);
-        if (existeNome) {
-            throw new Error('Já existe uma prateleira com esse nome');
-        }
-        const result = await prateleiraRepositorio.criarPrateleira({descricaoPrateleira, nomePrateleira});
-        return result
+  async execute({
+    descricaoPrateleira,
+    nomePrateleira,
+  }: DadosPrateleira): Promise<prateleiras> {
+    const prateleiraRepositorio = new PrateleiraRepositorio();
+    const existeNome = await prateleiraRepositorio.listarUmaPrateleiraPeloNome(
+      nomePrateleira
+    );
+    if (existeNome) {
+      throw new Error("Já existe uma prateleira com esse nome");
     }
-
+    const result = await prateleiraRepositorio.criarPrateleira({
+      descricaoPrateleira,
+      nomePrateleira,
+    });
+    return result;
+  }
 }
-export {CriarPrateleiraCasoDeUso}
+export { CriarPrateleiraCasoDeUso };
--- src/model/prateleiras/casoDeUso/criarPrateleira/CriarPrateleiraController.ts
+++ src/model/prateleiras/casoDeUso/criarPrateleira/CriarPrateleiraController.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import { CriarPrateleiraCasoDeUso } from "./CriarPrateleiraCasoDeUso";
 
 class CriarPrateleiraController {
-    async handle(req: Request, res: Response): Promise<any> {
-        const prateleiraCasoDeUso = new CriarPrateleiraCasoDeUso();
-        const { descricaoPrateleira, nomePrateleira } = req.body;
-        const result = await prateleiraCasoDeUso.execute({ descricaoPrateleira, nomePrateleira });
-        return res.status(201).json(result);
-    }
+  async handle(req: Request, res: Response): Promise<any> {
+    const prateleiraCasoDeUso = new CriarPrateleiraCasoDeUso();
+    const { descricaoPrateleira, nomePrateleira } = req.body;
+    const result = await prateleiraCasoDeUso.execute({
+      descricaoPrateleira,
+      nomePrateleira,
+    });
+    return res.status(201).json(result);
+  }
 }
 
-export { CriarPrateleiraController }
+export { CriarPrateleiraController };
