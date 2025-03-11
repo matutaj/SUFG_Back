994285e447e327b042681510057a7bc02ae48e97
matutaj <matutajorge2002@gmail.com>
2025-03-11 21:44:08 +0100

    routa corredor

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { clientesRoutes } from "./cliente.routes";
+import { corredorRoutes } from "./corredor.routes";
 const routes = Router();
 
 routes.use("/Clientes", clientesRoutes);
+routes.use("/Corredores", corredorRoutes);
+
 export { routes };
