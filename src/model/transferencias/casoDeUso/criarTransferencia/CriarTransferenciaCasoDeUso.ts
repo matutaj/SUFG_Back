8b6d606ba7b4f97656ec3447b320fc7dfd6ebc5b
matutaj <matutajorge2002@gmail.com>
2025-03-18 00:46:37 +0100

    Correção de erro no metodo de criação da transferencia

--- src/model/transferencias/casoDeUso/criarTransferencia/CriarTransferenciaCasoDeUso.ts
+++ src/model/transferencias/casoDeUso/criarTransferencia/CriarTransferenciaCasoDeUso.ts
@@ -1,15 +1,28 @@
-import { transferencias } from "@prisma/client"
-import { DadosTransferencia } from "../../repositorioTransferencia/ITransferencia"
-import { TransferenciaRepositorio } from "../../repositorioTransferencia/implementacoes/RepositorioTransferencia"
+import { transferencias } from "@prisma/client";
+import { DadosTransferencia } from "../../repositorioTransferencia/ITransferencia";
+import { TransferenciaRepositorio } from "../../repositorioTransferencia/implementacoes/RepositorioTransferencia";
 class CriarTransferenciaCasoDeUso {
-    async execute({id_funcionario, id_localizacao, id_produto, dataTransferencia, quantidadeTransferida}: DadosTransferencia): Promise<transferencias> {
-        const repositorioTransferencia = new TransferenciaRepositorio()
-        const existeData = await repositorioTransferencia.listarTodasTransferencias()
-        if (!existeData) {
-            throw new Error("Nenhuma transferencia cadastrada")
-        }
-        const result = await repositorioTransferencia.criarTransferencia({id_funcionario, id_localizacao, id_produto, dataTransferencia, quantidadeTransferida})
-        return result
+  async execute({
+    id_funcionario,
+    id_localizacao,
+    id_produto,
+    dataTransferencia,
+    quantidadeTransferida,
+  }: DadosTransferencia): Promise<transferencias> {
+    const repositorioTransferencia = new TransferenciaRepositorio();
+    const existeData =
+      await repositorioTransferencia.listarTodasTransferencias();
+    if (!existeData) {
+      throw new Error("Nenhuma transferencia cadastrada");
     }
+    const result = await repositorioTransferencia.criarTransferencia({
+      id_funcionario,
+      id_localizacao,
+      id_produto,
+      dataTransferencia,
+      quantidadeTransferida,
+    });
+    return result;
+  }
 }
-export {CriarTransferenciaCasoDeUso}
+export { CriarTransferenciaCasoDeUso };
--- src/model/transferencias/casoDeUso/criarTransferencia/CriarTransferenciaController.ts
+++ src/model/transferencias/casoDeUso/criarTransferencia/CriarTransferenciaController.ts
@@ -1,11 +1,23 @@
-import { CriarTransferenciaCasoDeUso } from "./CriarTransferenciaCasoDeUso"
-import { Request, Response } from "express"
-class CriarTransferenciaController{
-    async handle (req: Request, res: Response): Promise<any>{
-        const criarTransferenciaCasoDeUso = new CriarTransferenciaCasoDeUso()
-        const {id_funcionario, id_localizacao, id_produto, dataTransferencia, quantidadeTransferida} = req.body
-        const result = await criarTransferenciaCasoDeUso.execute({id_funcionario, id_localizacao, id_produto, dataTransferencia, quantidadeTransferida})
-        return res.status(201).json(result)
-    }
+import { CriarTransferenciaCasoDeUso } from "./CriarTransferenciaCasoDeUso";
+import { Request, Response } from "express";
+class CriarTransferenciaController {
+  async handle(req: Request, res: Response): Promise<any> {
+    const criarTransferenciaCasoDeUso = new CriarTransferenciaCasoDeUso();
+    const {
+      id_funcionario,
+      id_localizacao,
+      id_produto,
+      dataTransferencia,
+      quantidadeTransferida,
+    } = req.body;
+    const result = await criarTransferenciaCasoDeUso.execute({
+      id_funcionario,
+      id_localizacao,
+      id_produto,
+      dataTransferencia,
+      quantidadeTransferida,
+    });
+    return res.status(201).json(result);
+  }
 }
-export {CriarTransferenciaController}
+export { CriarTransferenciaController };
