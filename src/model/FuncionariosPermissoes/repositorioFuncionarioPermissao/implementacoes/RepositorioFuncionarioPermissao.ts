52a50db0275cba491ecf16555f6565c3005a7419
matutaj <matutajorge2002@gmail.com>
2025-03-17 10:43:25 +0100

    yuiuyi

--- src/model/FuncionariosPermissoes/repositorioFuncionarioPermissao/implementacoes/RepositorioFuncionarioPermissao.ts
+++ src/model/FuncionariosPermissoes/repositorioFuncionarioPermissao/implementacoes/RepositorioFuncionarioPermissao.ts
@@ -1,32 +1,50 @@
 import { funcionariosPermissoes } from "@prisma/client";
 import { prisma } from "../../../../prisma/client";
-import { DadosFuncionarioPermissao, IFuncionarioPermissao } from "../IFuncionarioPermissao";
+import {
+  DadosFuncionarioPermissao,
+  IFuncionarioPermissao,
+} from "../IFuncionarioPermissao";
 
 class FuncionarioPermissaoRepositorio {
-    async criarFuncionarioPermissao({id_funcionario, id_permissao}: DadosFuncionarioPermissao): Promise<funcionariosPermissoes> {
-        const funcionarioPermissao = await prisma.funcionariosPermissoes.create({ data: { id_funcionario, id_permissao } });
-        return funcionarioPermissao;
-    }
-    async listarTodosFuncionariosPermissoes(): Promise<funcionariosPermissoes[]> {
-        const listarTodosFuncionariosPermissoes = await prisma.funcionariosPermissoes.findMany();
-        return listarTodosFuncionariosPermissoes;
-    }
-    async listarUmFuncionarioPermissaoPeloId(id: string): Promise<funcionariosPermissoes | undefined> {
-        const listarUmFuncionarioPermissaoPeloId = await prisma.funcionariosPermissoes.findUnique({ where: { id } }) || undefined
-        return listarUmFuncionarioPermissaoPeloId;
-    }
-    async atualizarFuncionarioPermissao({id_funcionario, id_permissao, id}: DadosFuncionarioPermissao): Promise<funcionariosPermissoes>{
-        const atualizarFuncionarioPermissao = await prisma.funcionariosPermissoes.update({
-            where: {id},
-            data: {
-                id_funcionario,
-                id_permissao
-            }
-        })
-        return atualizarFuncionarioPermissao
-    }
-    async eliminarFuncionarioPermissao(id: string): Promise<void>{
-        await prisma.funcionariosPermissoes.delete({where:{id}})
-    }
+  async criarFuncionarioPermissao({
+    id_funcionario,
+    id_permissao,
+  }: DadosFuncionarioPermissao): Promise<funcionariosPermissoes> {
+    const funcionarioPermissao = await prisma.funcionariosPermissoes.create({
+      data: { id_funcionario, id_permissao },
+    });
+    return funcionarioPermissao;
+  }
+  async listarTodosFuncionariosPermissoes(): Promise<funcionariosPermissoes[]> {
+    const listarTodosFuncionariosPermissoes =
+      await prisma.funcionariosPermissoes.findMany();
+    return listarTodosFuncionariosPermissoes;
+  }
+  async listarUmFuncionarioPermissaoPeloId(
+    id: string
+  ): Promise<funcionariosPermissoes | undefined> {
+    const listarUmFuncionarioPermissaoPeloId =
+      (await prisma.funcionariosPermissoes.findUnique({ where: { id } })) ||
+      undefined;
+    return listarUmFuncionarioPermissaoPeloId;
+  }
+  async atualizarFuncionarioPermissao({
+    id_funcionario,
+    id_permissao,
+    id,
+  }: DadosFuncionarioPermissao): Promise<funcionariosPermissoes> {
+    const atualizarFuncionarioPermissao =
+      await prisma.funcionariosPermissoes.update({
+        where: { id },
+        data: {
+          id_funcionario,
+          id_permissao,
+        },
+      });
+    return atualizarFuncionarioPermissao;
+  }
+  async eliminarFuncionarioPermissao(id: string): Promise<void> {
+    await prisma.funcionariosPermissoes.delete({ where: { id } });
+  }
 }
-export {FuncionarioPermissaoRepositorio}
+export { FuncionarioPermissaoRepositorio };
--- src/model/funcionariosCaixa/repositorioFuncionarioCaixa/implementacoes/RepositorioFuncionarioCaixa.ts
+++ src/model/funcionariosCaixa/repositorioFuncionarioCaixa/implementacoes/RepositorioFuncionarioCaixa.ts
@@ -3,16 +3,37 @@ import { DadosFuncionarioCaixa } from "../IFuncionarioCaixa";
 import { prisma } from "../../../../prisma/client";
 
 class FuncionarioCaixaRepositorio {
-    async criarFuncionarioCaixa({estadoCaixa, horarioAbertura, horarioFechamento, quantidadaFaturada, id_caixa, id_funcionario}: DadosFuncionarioCaixa): Promise<funcionariosCaixa> {
-        return await prisma.funcionariosCaixa.create({ data: { estadoCaixa, horarioAbertura, horarioFechamento, quantidadaFaturada, id_caixa, id_funcionario } });
-    }
-    async listarTodosFuncionariosCaixa(): Promise<funcionariosCaixa[]> {
-        const listarTodosFuncionariosCaixa = await prisma.funcionariosCaixa.findMany();
-        return listarTodosFuncionariosCaixa;
-    }
-    async listarUmFuncionarioCaixaPeloId(id: string): Promise<funcionariosCaixa | undefined> {
-        const listarUmFuncionarioCaixaPeloId = await prisma.funcionariosCaixa.findUnique({ where: { id } }) || undefined
-        return listarUmFuncionarioCaixaPeloId;
-    }
+  async criarFuncionarioCaixa({
+    estadoCaixa,
+    horarioAbertura,
+    horarioFechamento,
+    quantidadaFaturada,
+    id_caixa,
+    id_funcionario,
+  }: DadosFuncionarioCaixa): Promise<funcionariosCaixa> {
+    return await prisma.funcionariosCaixa.create({
+      data: {
+        estadoCaixa,
+        horarioAbertura,
+        horarioFechamento,
+        quantidadaFaturada,
+        id_caixa,
+        id_funcionario,
+      },
+    });
+  }
+  async listarTodosFuncionariosCaixa(): Promise<funcionariosCaixa[]> {
+    const listarTodosFuncionariosCaixa =
+      await prisma.funcionariosCaixa.findMany();
+    return listarTodosFuncionariosCaixa;
+  }
+  async listarUmFuncionarioCaixaPeloId(
+    id: string
+  ): Promise<funcionariosCaixa | undefined> {
+    const listarUmFuncionarioCaixaPeloId =
+      (await prisma.funcionariosCaixa.findUnique({ where: { id } })) ||
+      undefined;
+    return listarUmFuncionarioCaixaPeloId;
+  }
 }
 export { FuncionarioCaixaRepositorio };
