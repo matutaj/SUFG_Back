396e725d8bffc78c99d51952d790bbe68d354d88
matutaj <matutajorge2002@gmail.com>
2025-03-17 21:58:36 +0100

    add

--- src/model/categoriaProdutos/casoDeUso/listarTodasCategoriasProdutos/ListarTodasCategoriasProdutosController.ts
+++ src/model/categoriaProdutos/casoDeUso/listarTodasCategoriasProdutos/ListarTodasCategoriasProdutosController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { ListarTodasCategoriasProdutosCasoDeUso } from "./ListarTodasCategoriasProdutosCasoDeUso";  
 
 class ListarTodasCategoriasProdutosController {
-    async handle(req: Request, res: Response) {
+    async handle(req: Request, res: Response) :Promise<any> {
         const listarTodasCategoriasProdutosCasoDeUso = new ListarTodasCategoriasProdutosCasoDeUso();
         const result = await listarTodasCategoriasProdutosCasoDeUso.execute();  
         return res.status(201).json(result);
