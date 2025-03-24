ee0c38ffd5ce5b3125f4b3fb14116f72743ee8f1
matutaj <matutajorge2002@gmail.com>
2025-03-24 09:41:10 +0100

    add

--- src/model/localizacoes/casoDeUso/listarLocalizacaoPeloNome/ListarLocalizacaoPeloNomeController.ts
+++ src/model/localizacoes/casoDeUso/listarLocalizacaoPeloNome/ListarLocalizacaoPeloNomeController.ts
@@ -2,9 +2,12 @@ import { Request, Response } from "express";
 import { ListarUmLocalizacaoPeloNomeCasoDeUso } from "./ListarLocalizacaoPeloNomeCasoDeUso";
 class ListarUmLocalizacaoPeloNomeController {
   async handle(req: Request, res: Response): Promise<any> {
-    const listarUmLocalizacaoPeloNomeCasoDeUso = new ListarUmLocalizacaoPeloNomeCasoDeUso();
+    const listarUmLocalizacaoPeloNomeCasoDeUso =
+      new ListarUmLocalizacaoPeloNomeCasoDeUso();
     const { nomeLocalizacao } = req.query;
-    const result = await listarUmLocalizacaoPeloNomeCasoDeUso.execute(nomeLocalizacao as string);
+    const result = await listarUmLocalizacaoPeloNomeCasoDeUso.execute(
+      nomeLocalizacao as string
+    );
     return res.status(200).json(result);
   }
 }
