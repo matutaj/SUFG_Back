0b46751956defc103698af02dacc5a916cb84b36
matutaj <matutajorge2002@gmail.com>
2025-03-25 16:30:20 +0100

    add

--- src/model/prateleiras/casoDeUso/criarPrateleira/CriarPrateleiraController.ts
+++ src/model/prateleiras/casoDeUso/criarPrateleira/CriarPrateleiraController.ts
@@ -5,15 +5,9 @@ import { AppError } from "../../../../errors/AppError";
 class CriarPrateleiraController {
   async handle(req: Request, res: Response): Promise<any> {
     const prateleiraCasoDeUso = new CriarPrateleiraCasoDeUso();
-<<<<<<< HEAD
-    const { descricaoPrateleira, nomePrateleira } = req.body;
-    if (!criarPrateleiraSchema.isValid(req.body))
-      throw new AppError("Erro na Validação dos dados");
-=======
     const { descricao, nomePrateleira } = req.body;
     if (!criarPrateleiraSchema.validate({ descricao, nomePrateleira }))
       return res.status(400).json({ message: "Dados inválidos" });
->>>>>>> 8b4f9671114a7378a553c1a8923114acc7613d05
     const result = await prateleiraCasoDeUso.execute({
       descricao,
       nomePrateleira,
