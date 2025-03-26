97cdda1b124cc7e12756682fbeff5c8497f54418
matutaj <matutajorge2002@gmail.com>
2025-03-26 13:01:36 +0100

    add

--- src/model/vendasProdutos/casoDeUso/listarVendaProdutoPeloId/ListarVendaProdutoPeloIdController.ts
+++ src/model/vendasProdutos/casoDeUso/listarVendaProdutoPeloId/ListarVendaProdutoPeloIdController.ts
@@ -4,9 +4,11 @@ import { listarVendaProdutoPeloIdSchema } from "../../../../schema/vendasProduto
 import { AppError } from "../../../../errors/AppError";
 class ListarVendaProdutoPorIdController {
   async handle(req: Request, res: Response): Promise<any> {
-    const listarVendaProdutoPorIdCasoDeUso = new ListarVendaProdutoPorIdCasoDeUso();
+    const listarVendaProdutoPorIdCasoDeUso =
+      new ListarVendaProdutoPorIdCasoDeUso();
     const { id } = req.params;
-    if (!listarVendaProdutoPeloIdSchema.isValid(req.params)) throw new AppError("Erro na Validação dos dados");
+    if (!listarVendaProdutoPeloIdSchema.isValid(req.params))
+      throw new AppError("Erro na Validação dos dados");
     const result = await listarVendaProdutoPorIdCasoDeUso.execute(id);
     return res.status(200).json(result);
   }
