70940ad1d4b4d986c83a6a153b56bb2657ecba44
matutaj <matutajorge2002@gmail.com>
2025-03-27 14:55:53 +0100

    add

--- src/model/clientes/casoDeUso/criarCliente/CriarClienteCasoDeUso.ts
+++ src/model/clientes/casoDeUso/criarCliente/CriarClienteCasoDeUso.ts
@@ -1,25 +1,42 @@
 import { clientes } from "@prisma/client";
 import { DadosCliente } from "../../repositorioCliente/ICliente";
 import { ClienteRepositorio } from "../../repositorioCliente/implementacoes/RepositorioCliente";
+import { AppError } from "../../../../errors/AppError";
 
-class CriarClienteCasoDeUso{
-    async execute({emailCliente, moradaCliente, nomeCliente, numeroContribuinte, telefoneCliente}: DadosCliente): Promise<clientes> {
-        const repositorioCliente = new ClienteRepositorio()
-        const existeEmail = await repositorioCliente.listarEmailCliente(emailCliente)
-        if(existeEmail){
-            throw new Error("Já existe um cliente com esse email")
-        }
-        const existeContribuinte= await repositorioCliente.listarNumeroDeContribuinte(numeroContribuinte)
-        if(existeContribuinte){
-            throw new Error("Já existe um cliente com esse número de contribuinte")
-        }
-        const existeTelefone= await repositorioCliente.listarTelefoneCliente(telefoneCliente)
-        if(existeTelefone){
-            throw new Error("Já existe um cliente com esse telefone")
-        }
-        const result = await repositorioCliente.criarCliente({emailCliente, moradaCliente, nomeCliente, numeroContribuinte, telefoneCliente})
-        return result
+class CriarClienteCasoDeUso {
+  async execute({
+    emailCliente,
+    moradaCliente,
+    nomeCliente,
+    numeroContribuinte,
+    telefoneCliente,
+  }: DadosCliente): Promise<clientes> {
+    const repositorioCliente = new ClienteRepositorio();
+    const existeEmail = await repositorioCliente.listarEmailCliente(
+      emailCliente
+    );
+    if (existeEmail) {
+      throw new AppError("Já existe um cliente com esse email");
     }
-
+    const existeContribuinte =
+      await repositorioCliente.listarNumeroDeContribuinte(numeroContribuinte);
+    if (existeContribuinte) {
+      throw new Error("Já existe um cliente com esse número de contribuinte");
+    }
+    const existeTelefone = await repositorioCliente.listarTelefoneCliente(
+      telefoneCliente
+    );
+    if (existeTelefone) {
+      throw new Error("Já existe um cliente com esse telefone");
+    }
+    const result = await repositorioCliente.criarCliente({
+      emailCliente,
+      moradaCliente,
+      nomeCliente,
+      numeroContribuinte,
+      telefoneCliente,
+    });
+    return result;
+  }
 }
-export {CriarClienteCasoDeUso}
+export { CriarClienteCasoDeUso };
--- src/model/clientes/casoDeUso/criarCliente/CriarClienteController.ts
+++ src/model/clientes/casoDeUso/criarCliente/CriarClienteController.ts
@@ -1,17 +1,28 @@
-import { Request, Response } from "express"
-import { CriarClienteCasoDeUso } from "./CriarClienteCasoDeUso"
-import { criarClienteSchema } from "../../../../schema/clientes/"
+import { Request, Response } from "express";
+import { CriarClienteCasoDeUso } from "./CriarClienteCasoDeUso";
+import { criarClienteSchema } from "../../../../schema/clientes/";
 import { AppError } from "../../../../errors/AppError";
-class CriarClienteController{
-    async handle(req: Request, res: Response): Promise<any> {
-        const clienteCasoDeUso = new CriarClienteCasoDeUso()
-        const {emailCliente, moradaCliente, nomeCliente, numeroContribuinte, telefoneCliente} = req.body
-        if(! criarClienteSchema.isValid(req.body)){
-            throw new AppError("Erro na validação dos campos")
-        }
-        const result = await clienteCasoDeUso.execute({emailCliente, moradaCliente, nomeCliente, numeroContribuinte, telefoneCliente})
-        return res.status(201).json(result)
+class CriarClienteController {
+  async handle(req: Request, res: Response): Promise<any> {
+    const clienteCasoDeUso = new CriarClienteCasoDeUso();
+    const {
+      emailCliente,
+      moradaCliente,
+      nomeCliente,
+      numeroContribuinte,
+      telefoneCliente,
+    } = req.body;
+    if (!criarClienteSchema.isValid(req.body)) {
+      throw new AppError("Erro na validação dos campos");
     }
-    
+    const result = await clienteCasoDeUso.execute({
+      emailCliente,
+      moradaCliente,
+      nomeCliente,
+      numeroContribuinte,
+      telefoneCliente,
+    });
+    return res.status(201).json(result);
+  }
 }
-export {CriarClienteController}
+export { CriarClienteController };
