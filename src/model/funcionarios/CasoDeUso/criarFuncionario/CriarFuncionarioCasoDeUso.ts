585e00e535511caa0bcd8f222f166c149867dbcf
matutaj <matutajorge2002@gmail.com>
2025-04-01 11:21:53 +0100

    cryptografia da senha

--- src/model/funcionarios/CasoDeUso/criarFuncionario/CriarFuncionarioCasoDeUso.ts
+++ src/model/funcionarios/CasoDeUso/criarFuncionario/CriarFuncionarioCasoDeUso.ts
@@ -2,24 +2,51 @@ import { funcionarios } from "@prisma/client";
 import { DadosFuncionario } from "../../repositorioFuncionario/IFuncionario";
 import { FuncionarioRepositorio } from "../../repositorioFuncionario/implementacoes/RepositorioFuncionario";
 import { AppError } from "../../../../errors/AppError";
+import bcypt from "bcrypt";
 
 class CriarFuncionarioCasoDeUso {
-    async execute({emailFuncionario, moradaFuncionario, nomeFuncionario, numeroBI, telefoneFuncionario, senha}: DadosFuncionario): Promise<funcionarios> {
-        const repositorioFuncionario = new FuncionarioRepositorio();
-        const existeEmail = await repositorioFuncionario.listarEmailFuncionario(emailFuncionario);
-        if (existeEmail) {
-            throw new AppError("Já existe um funcionário com esse email");
-        }
-        const existeContribuinte = await repositorioFuncionario.listarNumeroContribuinteFuncionario(numeroBI);
-        if (existeContribuinte) {
-            throw new AppError("Já existe um funcionário com esse número de contribuinte");
-        }
-        const existeTelefone = await repositorioFuncionario.listarTelefoneFuncionario(telefoneFuncionario);
-        if (existeTelefone) {
-            throw new AppError("Já existe um funcionário com esse telefone");
-        }
-        const result = await repositorioFuncionario.criarFuncionario({emailFuncionario, moradaFuncionario, nomeFuncionario, numeroBI, telefoneFuncionario, senha});
-        return result;
+  async execute({
+    emailFuncionario,
+    moradaFuncionario,
+    nomeFuncionario,
+    numeroBI,
+    telefoneFuncionario,
+    senha,
+  }: DadosFuncionario): Promise<funcionarios> {
+    const repositorioFuncionario = new FuncionarioRepositorio();
+    const existeEmail = await repositorioFuncionario.listarEmailFuncionario(
+      emailFuncionario
+    );
+    if (existeEmail) {
+      throw new AppError("Já existe um funcionário com esse email");
     }
+    const existeContribuinte =
+      await repositorioFuncionario.listarNumeroContribuinteFuncionario(
+        numeroBI
+      );
+    if (existeContribuinte) {
+      throw new AppError(
+        "Já existe um funcionário com esse número de contribuinte"
+      );
+    }
+    const existeTelefone =
+      await repositorioFuncionario.listarTelefoneFuncionario(
+        telefoneFuncionario
+      );
+    if (existeTelefone) {
+      throw new AppError("Já existe um funcionário com esse telefone");
+    }
+
+    const cryptografarSenha = await bcypt.hash(senha, 10);
+    const result = await repositorioFuncionario.criarFuncionario({
+      emailFuncionario,
+      moradaFuncionario,
+      nomeFuncionario,
+      numeroBI,
+      telefoneFuncionario,
+      senha: cryptografarSenha,
+    });
+    return result;
+  }
 }
-export {CriarFuncionarioCasoDeUso}
+export { CriarFuncionarioCasoDeUso };
--- src/model/funcionarios/CasoDeUso/criarFuncionario/CriarFuncionarioController.ts
+++ src/model/funcionarios/CasoDeUso/criarFuncionario/CriarFuncionarioController.ts
@@ -3,13 +3,27 @@ import { Request, Response } from "express";
 import { criarFuncionarioSchema } from "../../../../schema/funcionarios";
 import { AppError } from "../../../../errors/AppError";
 class criarFuncionarioController {
-    async handle(req: Request, res: Response): Promise<any> {
-        const criarFuncionarioCasoDeUso = new CriarFuncionarioCasoDeUso();
-        const { nomeFuncionario, emailFuncionario, telefoneFuncionario, moradaFuncionario, senha, numeroBI } = req.body;
-        if (!criarFuncionarioSchema.isValid(req.body)) throw new AppError("Erro na Validação dos dados");
-        const result = await criarFuncionarioCasoDeUso.execute({ nomeFuncionario, emailFuncionario, telefoneFuncionario, moradaFuncionario, senha, numeroBI });
-        return res.status(201).json(result);
-    }
-
+  async handle(req: Request, res: Response): Promise<any> {
+    const criarFuncionarioCasoDeUso = new CriarFuncionarioCasoDeUso();
+    const {
+      nomeFuncionario,
+      emailFuncionario,
+      telefoneFuncionario,
+      moradaFuncionario,
+      senha,
+      numeroBI,
+    } = req.body;
+    if (!criarFuncionarioSchema.isValid(req.body))
+      throw new AppError("Erro na Validação dos dados");
+    const result = await criarFuncionarioCasoDeUso.execute({
+      nomeFuncionario,
+      emailFuncionario,
+      telefoneFuncionario,
+      moradaFuncionario,
+      senha,
+      numeroBI,
+    });
+    return res.status(201).json(result);
+  }
 }
-export {criarFuncionarioController}
+export { criarFuncionarioController };
