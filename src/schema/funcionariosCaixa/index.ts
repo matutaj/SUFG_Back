570052fd6982f35136f36e7b111beaa2ddc62c12
matutaj <matutajorge2002@gmail.com>
2025-04-03 12:09:26 +0100

    mudando os schema

--- src/schema/funcionariosCaixa/index.ts
+++ src/schema/funcionariosCaixa/index.ts
@@ -3,19 +3,18 @@ import * as Yup from "yup";
 const criarFuncionarioCaixaSchema = Yup.object().shape({
   id_caixa: Yup.string().required(),
   id_funcionario: Yup.string().required(),
-  quantidadaFaturada: Yup.number().required(),
+  quantidadaFaturada: Yup.number(),
   horarioAbertura: Yup.date().required(),
-  horarioFechamento: Yup.date().required(),
+  horarioFechamento: Yup.date(),
 });
 const listarFuncionarioCaixaPeloIdSchema = Yup.object().shape({
   id_funcionarioCaixa: Yup.string().required(),
 })
 const atualizarFuncionarioCaixaSchema = Yup.object().shape({
-  id_funcionarioCaixa: Yup.string().required(),
   id_caixa: Yup.string().required(),
   id_funcionario: Yup.string().required(),
   quantidadaFaturada: Yup.number().required(),
-  horarioAbertura: Yup.date().required(),
+  horarioAbertura: Yup.date(),
   horarioFechamento: Yup.date().required(),
 })
 const deletarFuncionarioCaixaSchema = Yup.object().shape({
--- src/schema/vendas/index.ts
+++ src/schema/vendas/index.ts
@@ -2,10 +2,10 @@ import * as Yup from "yup";
 
 const criarVendaSchema = Yup.object().shape({
   tipoDocumento: Yup.string(),
-  numeroDocumento: Yup.string().required(),
-  id_funcionarioCaixa: Yup.string().required(),
+  numeroDocumento: Yup.string(),
+  id_funcionarioCaixa: Yup.string(),
   id_cliente: Yup.string(),
-  dataEmissao: Yup.date().required(),
+  dataEmissao: Yup.date(),
   valorTotal: Yup.number(),
   dataValidade: Yup.date(),
 });
