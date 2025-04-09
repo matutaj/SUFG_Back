fb5ad328562080e04882156cfae4daf66f236df2
matutaj <matutajorge2002@gmail.com>
2025-04-09 09:23:50 +0100

    add

--- src/model/localizacoes/casoDeUso/deleteLocalizacao/DeleteLocalizacaoController.ts
+++ src/model/localizacoes/casoDeUso/deleteLocalizacao/DeleteLocalizacaoController.ts
@@ -6,7 +6,8 @@ class DeleteLocalizacaoController {
   async handle(req: Request, res: Response): Promise<any> {
     const deleteLocalizacaoCasoDeUso = new DeleteLocalizacaoCasoDeUso();
     const { id } = req.params;
-    if (!await deletarLocalizacaoSchema.isValid(req.params)) throw new AppError("Erro na Validação dos dados");
+    if (!(await deletarLocalizacaoSchema.isValid(req.params)))
+      throw new AppError("Erro na Validação dos dados");
     const result = await deleteLocalizacaoCasoDeUso.execute(id);
     return res.status(201).json(result);
   }
