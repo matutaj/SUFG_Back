7fef824bcbae95001f062b296a7c61db0ecf2d42
matutaj <matutajorge2002@gmail.com>
2025-04-14 14:42:31 +0100

    add

--- prisma/migrations/20250404114231_tornar_null/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `funcionarioscaixa` MODIFY `horarioFechamento` DATETIME(3) NULL;
--- prisma/migrations/20250414133619_new_tables/migration.sql
+++ prisma/migrations/20250414133619_new_tables/migration.sql
@@ -28,6 +28,25 @@ CREATE TABLE `funcionarios` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `tarefas` (
+    `id` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `funcionariosTarefas` (
+    `id` VARCHAR(191) NOT NULL,
+    `id_funcionario` VARCHAR(191) NOT NULL,
+    `id_tarefa` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `categoriasProdutos` (
     `id` VARCHAR(191) NOT NULL,
     `nomeCategoria` VARCHAR(191) NOT NULL,
@@ -50,6 +69,19 @@ CREATE TABLE `produtos` (
     `unidadeMedida` VARCHAR(191) NOT NULL,
     `unidadeConteudo` VARCHAR(191) NOT NULL,
     `codigoBarras` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `estoques` (
+    `id` VARCHAR(191) NOT NULL,
+    `id_produto` VARCHAR(191) NOT NULL,
+    `quantidadeAtual` INTEGER NOT NULL,
+    `lote` VARCHAR(191) NOT NULL,
+    `dataValidadeLote` DATETIME(3) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -206,7 +238,7 @@ CREATE TABLE `funcionariosCaixa` (
     `estadoCaixa` BOOLEAN NOT NULL,
     `quantidadaFaturada` DECIMAL(10, 2) NOT NULL,
     `horarioAbertura` DATETIME(3) NOT NULL,
-    `horarioFechamento` DATETIME(3) NOT NULL,
+    `horarioFechamento` DATETIME(3) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -291,7 +323,16 @@ CREATE TABLE `_fornecedoresTofuncionariosCaixa` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
+ALTER TABLE `funcionariosTarefas` ADD CONSTRAINT `funcionariosTarefas_id_funcionario_fkey` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionarios`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `funcionariosTarefas` ADD CONSTRAINT `funcionariosTarefas_id_tarefa_fkey` FOREIGN KEY (`id_tarefa`) REFERENCES `tarefas`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `produtos` ADD CONSTRAINT `produtos_id_categoriaProduto_fkey` FOREIGN KEY (`id_categoriaProduto`) REFERENCES `categoriasProdutos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `estoques` ADD CONSTRAINT `estoques_id_produto_fkey` FOREIGN KEY (`id_produto`) REFERENCES `produtos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `entradasEstoque` ADD CONSTRAINT `entradasEstoque_id_fornecedor_fkey` FOREIGN KEY (`id_fornecedor`) REFERENCES `fornecedores`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
